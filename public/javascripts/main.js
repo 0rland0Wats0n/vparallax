d5cc92b41302f1f10f9acf8f639e2311a6a21687
Orlando Watson <ogw@orlandogwatson.com>
2017-02-14 03:45:33 -0500

    another one

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,12 +1,13 @@
 $(function() {
     // Default
-    jQuery.scrollSpeed(50, 2000);
+    jQuery.scrollSpeed(50, 1800);
 });
 
 $(window).scroll(function() {
 
     // capture scrolltop
     var wScroll = $(this).scrollTop();
+    var wHeight = $(this).height();
 
     //heading
     $('.heading-box').css({
@@ -15,5 +16,32 @@ $(window).scroll(function() {
 
     $('.heading-box .welcome').css({
         'transform': 'translate(0, -' + wScroll/16 + '%)'
+    });
+
+    if(wScroll > 50 && !$('.js-fade-when-top').hasClass('fade-out')) {
+        //hide scroll instruction
+        $('.js-fade-when-top').addClass('fade-out');
+    }
+
+    if(wScroll - $('.header-bottom-instructions').offset().top >= 0) {
+        if(!$('.header-bottom-instructions').hasClass('stick-to-top')) {
+            $('.header-bottom-instructions').toggleClass('stick-to-top');
+        }
+    }
+
+    if(wScroll <= wHeight - 250) {
+        //hide scroll instruction
+        $('.js-fade-when-top').removeClass('fade-out');
+        $('.header-bottom-instructions').removeClass('stick-to-top');
+    }
+
+    // sections
+    $('.author-quote').each(function() {
+
+        if(wScroll > $(this).offset().top - $(window).height()) {
+            $(this).css({
+                'transform': 'translate(0, ' + wScroll/24 + '%)'
+            });
+        }
     });
 });
