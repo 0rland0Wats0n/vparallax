d037775d5af68d4650c6ea8fec003c1c431e24dd
Orlando Watson <ogw@orlandogwatson.com>
2017-02-14 05:18:21 -0500

    done

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,42 +1,46 @@
-$(window).scroll(function() {
+$(document).ready(function() {
+    $("html").niceScroll({scrollspeed: 100});
 
-    // capture scrolltop
-    var wScroll = $(this).scrollTop();
-    var wHeight = $(this).height();
+    $(window).scroll(function() {
 
-    //heading
-    $('.heading-box').css({
-        'transform': 'translate(0, -' + wScroll/8 + '%)'
-    });
+        // capture scrolltop
+        var wScroll = $(this).scrollTop();
+        var wHeight = $(this).height();
 
-    $('.heading-box .welcome').css({
-        'transform': 'translate(0, -' + wScroll/16 + '%)'
-    });
+        //heading
+        $('.heading-box').css({
+            'transform': 'translate(0, -' + wScroll/8 + '%)'
+        });
 
-    if(wScroll > 50 && !$('.js-fade-when-top').hasClass('fade-out')) {
-        //hide scroll instruction
-        $('.js-fade-when-top').addClass('fade-out');
-    }
+        $('.heading-box .welcome').css({
+            'transform': 'translate(0, -' + wScroll/16 + '%)'
+        });
 
-    if(wScroll - $('.header-bottom-instructions').offset().top >= 0) {
-        if(!$('.header-bottom-instructions').hasClass('stick-to-top')) {
-            $('.header-bottom-instructions').toggleClass('stick-to-top');
+        if(wScroll > 50 && !$('.js-fade-when-top').hasClass('fade-out')) {
+            //hide scroll instruction
+            $('.js-fade-when-top').addClass('fade-out');
         }
-    }
 
-    if(wScroll <= wHeight - 250) {
-        //hide scroll instruction
-        $('.js-fade-when-top').removeClass('fade-out');
-        $('.header-bottom-instructions').removeClass('stick-to-top');
-    }
+        if(wScroll - $('.header-bottom-instructions').offset().top >= 0) {
+            if(!$('.header-bottom-instructions').hasClass('stick-to-top')) {
+                $('.header-bottom-instructions').toggleClass('stick-to-top');
+            }
+        }
 
-    // sections
-    // $('.author-quote').each(function() {
-    //
-    //     if(wScroll > $(this).offset().top - $(window).height()) {
-    //         $(this).css({
-    //             'transform': 'translate(0, ' + wScroll/24 + '%)'
-    //         });
-    //     }
-    // });
+        if(wScroll <= wHeight - 250) {
+            //hide scroll instruction
+            $('.js-fade-when-top').removeClass('fade-out');
+            $('.header-bottom-instructions').removeClass('stick-to-top');
+        }
+
+        // sections
+        // $('.author-quote').each(function() {
+        //
+        //     if(wScroll > $(this).offset().top - $(window).height()) {
+        //         $(this).css({
+        //             'transform': 'translate(0, ' + wScroll/24 + '%)'
+        //         });
+        //     }
+        // });
+    });
 });
